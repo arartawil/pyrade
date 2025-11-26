7b1a9c1f5a6a56fb3834618be9ad5651e976d23e
arar suliman al tawil <arartawil@yahoo.com>
2025-11-27 00:29:25 +0800

    chore: bump version to 0.2.0 - add visualization

--- pyrade/__version__.py
+++ pyrade/__version__.py
@@ -1,3 +1,3 @@
 """PyRADE package version."""
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="pyrade",
-    version="0.1.0",
+    version="0.2.0",
     author="PyRADE Contributors",
     author_email="arartawil@gmail.com",
     description="High-performance, modular Differential Evolution optimization",
